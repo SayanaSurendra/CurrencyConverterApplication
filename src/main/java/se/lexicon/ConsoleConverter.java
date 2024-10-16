d1b1ec2e1e7b87dfd6700812495fdee421c51c70
Sayana Surendran <sayanasurendran31@gmail.com>
2024-10-16 15:41:16 +0200

    Added utility file

--- src/main/java/se/lexicon/ConsoleConverter.java
+++ src/main/java/se/lexicon/ConsoleConverter.java
@@ -1,16 +1,14 @@
 package se.lexicon;
 
 import se.lexicon.constants.CurrencyRateConstants;
-
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import se.lexicon.utils.CurrencyConversionUtil;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ConsoleConverter {
 
     static void runConverter(){
-        displayMenu();
+       CurrencyConversionUtil. displayMenu();
         Scanner in= new Scanner(System.in).useLocale(Locale.US);
         int choice=in.nextInt();
         doConversion(choice,in);
@@ -24,28 +22,28 @@ public class ConsoleConverter {
                 System.out.println("Enter SEK amount to be converted to USD : ");
                 double sek= in.nextDouble();
                 double usdAmount=CurrencyConverter.conversion(sek, CurrencyRateConstants.SEK_TO_USD_RATE);
-                System.out.println(sek+" SEK = "+currencyFormat(usdAmount)+" USD");
+                System.out.println(sek+" SEK = "+CurrencyConversionUtil.currencyFormat(usdAmount)+" USD"+ " as on "+CurrencyConversionUtil.cuurentDate()+" "+CurrencyConversionUtil.currentTime());
                 break;
 
             case 2:
                 System.out.println("Enter USD amount to be converted to SEK : ");
                 double usd= in.nextDouble();
                 double sekAmount=CurrencyConverter.conversion(usd, CurrencyRateConstants.USD_TO_SEK_RATE);
-                System.out.println(usd+" USD = "+currencyFormat(sekAmount)+" SEK");
+                System.out.println(usd+" USD = "+CurrencyConversionUtil.currencyFormat(sekAmount)+" SEK"+ " as on "+CurrencyConversionUtil.cuurentDate()+" "+CurrencyConversionUtil.currentTime());
                 break;
 
             case 3:
                 System.out.println("Enter SEK amount to be converted to Euro : ");
                 double sekEuro= in.nextDouble();
                 double sekEuroAmount=CurrencyConverter.conversion(sekEuro, CurrencyRateConstants.SEK_TO_EURO_RATE);
-                System.out.println(sekEuro+" SEK = "+currencyFormat(sekEuroAmount)+" EUR" );
+                System.out.println(sekEuro+" SEK = "+CurrencyConversionUtil.currencyFormat(sekEuroAmount)+" EUR"+ " as on "+CurrencyConversionUtil.cuurentDate()+" "+CurrencyConversionUtil.currentTime() );
                 break;
 
             case 4:
                 System.out.println("Enter Euro amount to be converted to SEK : ");
                 double euroAmount= in.nextDouble();
-                double euroSekAmount=CurrencyConverter.conversion(euroAmount, CurrencyRateConstants.SEK_TO_EURO_RATE);
-                System.out.println(euroAmount+" SEK = "+currencyFormat(euroSekAmount)+" EUR");
+                double euroSekAmount=CurrencyConverter.conversion(euroAmount, CurrencyRateConstants.EURO_TO_SEK_RATE);
+                System.out.println(euroAmount+" EUR = "+CurrencyConversionUtil.currencyFormat(euroSekAmount)+" SEK"+ " as on "+CurrencyConversionUtil.cuurentDate()+" "+CurrencyConversionUtil.currentTime());
                 break;
 
             case 0:
@@ -57,23 +55,7 @@ public class ConsoleConverter {
         }
     }
 
-    private static String currencyFormat(double amount) {
-        NumberFormat localeAmountFormat=NumberFormat.getCurrencyInstance(Locale.US);
-        DecimalFormat decimalFormat = (DecimalFormat)localeAmountFormat;
-        decimalFormat.applyPattern("#,##0.00");
-        String format = decimalFormat.format(amount);
 
-         return  format;
 
-    }
 
-    private static void displayMenu() {
-        System.out.println("  Currency Converter App  ");
-        System.out.println("1. Convert SEK to USD");
-        System.out.println("2. Convert USD to SEK");
-        System.out.println("3. Convert SEK to Euro");
-        System.out.println("4. Convert Euro to SEK");
-        System.out.println("0. Exit");
-        System.out.println("Enter your choice:");
-    }
 }
---
+++ src/main/java/se/lexicon/utils/CurrencyConversionUtil.java
@@ -0,0 +1,46 @@
+package se.lexicon.utils;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class CurrencyConversionUtil {
+
+    public static void displayMenu() {
+        System.out.println("  Currency Converter App  ");
+        System.out.println("1. Convert SEK to USD");
+        System.out.println("2. Convert USD to SEK");
+        System.out.println("3. Convert SEK to Euro");
+        System.out.println("4. Convert Euro to SEK");
+        System.out.println("0. Exit");
+        System.out.println("Enter your choice:");
+    }
+
+    public static String currencyFormat(double amount) {
+        NumberFormat localeAmountFormat=NumberFormat.getCurrencyInstance(Locale.US);
+        DecimalFormat decimalFormat = (DecimalFormat)localeAmountFormat;
+        decimalFormat.applyPattern("#,##0.00");
+        String format = decimalFormat.format(amount);
+
+        return  format;
+
+    }
+
+    public static String cuurentDate(){
+        LocalDate dt=LocalDate.now();
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd MMMM");
+        String formattedDate = dt.format(formatter);
+        return formattedDate;
+
+    }
+
+    public static String currentTime(){
+        LocalTime currentTime=LocalTime.now();
+        DateTimeFormatter formattedTime=DateTimeFormatter.ofPattern("hh:mm a");
+        String formatTime= formattedTime.format(currentTime);
+        return formatTime;
+    }
+}
